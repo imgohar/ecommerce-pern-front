4aecd1cd4e41431a37a03ce89d4c2653dec40547
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2021-03-15 07:54:56 +0500

    first commit

---
+++ src/user/Signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../core/Layout";
+
+const Signin = () => {
+    return(
+        <Layout title="Signin" description="Signin to Node React E-comm app">
+            ...
+        </Layout>
+    );
+}
+
+
+export default Signin;
